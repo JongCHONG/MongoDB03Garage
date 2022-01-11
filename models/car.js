b888de8abf48efc535c8367face83841d387e869
JongCHONG <yonhoa27@hotmail.fr>
2022-01-11 17:01:33 +0100

    modèle car

---
+++ models/car.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+const carSchema = new mongoose.Schema({
+  brand: {
+    type:	String
+  },
+  model: {
+    type:	String
+  },
+  year:	{
+    type: Number
+  }
+},
+{ 
+  timestamps: true
+})
+
+const Car = mongoose.model('Car', carSchema)
+
+module.exports = Car
